760c71d3439620ad79d63541c494d3e1bcbfe879
Francis <yaowen021316@hotmail>
2021-10-12 13:19:11 +1100

    fix(*)register btn issue fixed

--- src/js/module/register.js
+++ src/js/module/register.js
@@ -1,7 +1,6 @@
 import { registerCheck } from '../utilities/http'
 export default class Register {
 	constructor(el) {
-		this.name = 'register form'
 		this.username = el.querySelectorAll('input')[0]
 		this.password = el.querySelectorAll('input')[1]
 		this.btn = el.querySelectorAll('button')[0]
@@ -14,6 +13,7 @@ export default class Register {
 	}
 
 	async onclick() {
+		console.log(123)
 		const userInfor = {
 			username: this.username.value,
 			password: this.password.value
@@ -21,6 +21,9 @@ export default class Register {
 		const check = await this.registerCheck({
 			...userInfor
 		})
+		if (check) {
+			alert('successfully')
+		}
 	}
 
 	registerCheck(userInfor) {
--- src/js/register.js
+++ src/js/register.js
@@ -1,6 +1,6 @@
 import Register from './module/register'
 window.addEventListener('load', () => {
-	const loginFrom = document.getElementsByClassName('loginForm')[0]
+	const loginFrom = document.getElementsByClassName('registerForm')[0]
 	const init = () => {
 		new Register(loginFrom).init()
 	}
